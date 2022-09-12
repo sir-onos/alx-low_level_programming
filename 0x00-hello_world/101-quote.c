7ad3745b1397ea117f73b391bf3883c836c78825
obote onos <103035394+sir-onos@users.noreply.github.com>
2022-09-12 23:33:25 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - prints out a string using write
-* Description: Program that prints out the string s followed by a new line
-* Return: int 0 when done and no errors.
-* Could not use any system calls. Or any standard library functions.
-* Used write tooutput to stderror. Could have tried a method with 2>
-* the bit field is 59. I tried 58, cut off, I tried 60, 61, and higher and
-* it just won't pass because there are prob other things in buffer
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
- write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
- , 59);
- return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
 }
