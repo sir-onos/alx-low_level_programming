a5feebb40be013755f856cd0c92f1e6405cf1004
obote onos <103035394+sir-onos@users.noreply.github.com>
2022-09-20 00:49:29 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
- #include "main.h"
+#include "main.h"
 
-  /**
-  * reset_to_98 - takes a pointer to an int as parameter and updates
-  * the value it points to to 98
-  * @n: pointer to an int
-  * Return: Nothing
-  */
+/**
+ * reset_to_98 - takes a pointer and updates the value it point to
+ * @n: pointer
+ * Return: void
+ */
 
- void reset_to_98(int *n)
- {
-	  *n = 98;
- }
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
